92c41351212655c5e79d168a8a0b4ce23d4a9cb6
Lucas <lucaszago.ph@gmail.com>
2022-08-30 15:39:39 +0200

    add audio

--- typewriter.js
+++ typewriter.js
@@ -2,9 +2,17 @@
 document.addEventListener("DOMContentLoaded", init);
 let str, newStr;
 let i = 0;
-
+let typeCarriageReturn, typekey1, typekey2, typelastkey, typespace;
+let audios;
 function init() {
-  console.log("init");
+  typeCarriageReturn = document.querySelector("typereturn");
+  typekey1 = document.querySelector("#typekey1");
+  typekey2 = document.querySelector("#typekey2");
+  typelastkey = document.querySelector("#typelast");
+  typespace = document.querySelector("#typespace");
+  document.querySelector("button").addEventListener("click", () => {
+    activateAudio();
+  });
   str = document.querySelector("#typewriter").textContent;
   newStr = document.createElement("h1");
   newStr.classList.add("typewritten");
@@ -16,26 +24,36 @@ function init() {
     return;
   }
 }
-
+function activateAudio() {}
 function loop() {
   //find character
   let curChar = str[i];
 
+  //audio
+  if (str[i] == " ") {
+    typespace.play();
+  } else if (i % 2 == 0) {
+    typekey1.play();
+  } else if (i % 2 == 1) {
+    typekey2.play();
+  } else if (i === str.length - 1) {
+    typelast.play();
+  }
   //add charater to main string
-  newStr.textContent += curChar;
+  newStr.innerHTML += curChar;
 
   //iterator++
   i++;
 
-  //   console.log(newStr.textContent);
   //exit case
   if (i < str.length) {
-    setTimeout(loop, Math.floor(Math.random() * 200) + 30);
+    setTimeout(loop, Math.floor(Math.random() * 300) + 30);
   } else {
     i = 0;
+    newStr.innerHTML += "<br />";
+    typelastkey.play();
     setTimeout(() => {
-      newStr.textContent = "";
       loop();
-    }, 1000);
+    }, 1800);
   }
 }
