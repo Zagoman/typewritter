f621d324ce2c383c39202b23c258b9595c6491b1
Lucas <lucaszago.ph@gmail.com>
2022-08-30 14:41:48 +0200

    TCW make typewriter work

---
+++ typewriter.js
@@ -0,0 +1,41 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", init);
+let str, newStr;
+let i = 0;
+
+function init() {
+  console.log("init");
+  str = document.querySelector("#typewriter").textContent;
+  newStr = document.createElement("h1");
+  newStr.classList.add("typewritten");
+  newStr.textContent = "";
+  document.querySelector("body").appendChild(newStr);
+  if (str) {
+    loop();
+  } else {
+    return;
+  }
+}
+
+function loop() {
+  //find character
+  let curChar = str[i];
+
+  //add charater to main string
+  newStr.textContent += curChar;
+
+  //iterator++
+  i++;
+
+  //   console.log(newStr.textContent);
+  //exit case
+  if (i < str.length) {
+    setTimeout(loop, 90);
+  } else {
+    i = 0;
+    setTimeout(() => {
+      newStr.textContent = "";
+      loop();
+    }, 1000);
+  }
+}
